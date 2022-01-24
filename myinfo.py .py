5aea0113a70efb773b3839f741f3ed7b3177ad8b
AdnanHoti <91210634+AdnanHoti@users.noreply.github.com>
2022-01-24 18:56:27 -0500

    Myinfo.py Version 2

---
+++ myinfo.py .py
@@ -0,0 +1,8 @@
+#CS175-50
+#Adnan Hoti
+#myInfo
+print("My name is Adnan Hoti")
+print("I live at 289 columbus ave Carlstadt, NJ, 07072")
+print("My phone nymber is 201-551-7879")
+print("My major is software engineering")
+
