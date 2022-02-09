6b37e47cf6d2f185b71bdc8175521ba24494a269
AdnanHoti <91210634+AdnanHoti@users.noreply.github.com>
2022-02-09 18:26:20 -0500

    Delete Sales+prediction2+.py

--- Sales+prediction2+.py
+++
@@ -1,7 +0,0 @@
-#Sales Prediction-Adnan Hoti
-annualprofitpercentage = 0.23
-projectedAmount=float(input("Enter the projected amount of total sales:"))
-profit = projectedAmount * annualprofitpercentage
-print(profit)
-
-
